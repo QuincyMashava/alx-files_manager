4e75c6cf21c836aa674e531c65bfaca60e8cd7af
QuincyMashava <mashavaquincy@gmail.com>
2022-08-25 02:08:46 +0200

    redis.js correction

--- utils/redis.js
+++ utils/redis.js
@@ -1,49 +1,3 @@
-<<<<<<< HEAD
-import { createClient } from 'redis';
-import { promisify } from 'util';
-
-class RedisClient {
-  constructor () {
-    this.redisClient = createClient({
-      host: process.env.REDIS_HOST,
-      port: process.env.REDIS_PORT,
-      password: process.env.REDIS_PASSWORD
-    });
-    this.redisClient.on('error', (error) => {
-      console.error(error);
-    });
-    // this.redisClient.on('connect', () => {
-    //   console.log('Redis client connected');
-    // });
-  }
-
-  getRedisClient () {
-    return this.redisClient;
-  }
-
-  isAlive () {
-    return this.redisClient.connected;
-  }
-
-  async get (key) {
-    const getAsync = promisify(this.redisClient.get).bind(this.redisClient);
-    return await getAsync(key);
-  }
-
-  async set (key, value, duration) {
-    const setAsync = promisify(this.redisClient.set).bind(this.redisClient);
-    await setAsync(key, value, "EX", duration);
-  }
-
-  async del (key) {
-    const delAsync = promisify(this.redisClient.del).bind(this.redisClient);
-    await delAsync(key);
-  }
-}
-
-const redisClient = new RedisClient();
-
-=======
 import { promisify } from 'util';
 import { createClient } from 'redis';
 
@@ -106,5 +60,4 @@ class RedisClient {
 }
 
 export const redisClient = new RedisClient();
->>>>>>> 67881231 (Commitment for the first time)
 export default redisClient;
